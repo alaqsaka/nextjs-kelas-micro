f7e512665f11d9ffa2bb0b129491d834f51e8539
alaqsaka <aqsaabidin15@gmail.com>
2022-10-07 22:33:43 +0700

    initial setup

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require("next-compose-plugins");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withPlugins(
+  [
+    withImages({}),
+    withReactSvg({
+      include: path.resolve(__dirname, "./public/images"),
+      webpack(config, options) {
+        return config;
+      },
+    }),
+  ],
+  nextConfig
+);
